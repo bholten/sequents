d0d7c367ec8e6ff297cb62871e77f11a7436e61b
Brennan Holten <brennan@telsasoft.com>
2019-02-16 21:39:07 -0600

    Updated tests.

--- tests/automaton.rkt
+++ tests/automaton.rkt
@@ -1,20 +1,37 @@
-;; Test:
+#lang racket
+
+(require "../automaton.rkt")
+
+; Test:
+; Example from stackoverflow
+; https://codereview.stackexchange.com/questions/143726/event-driven-finite-state-machine-dsl-in-kotlin
+(define elevator (automaton
+                  idle
+                  [idle : (go-up   -> going-up)
+                          (go-down -> going-down)
+                          (at-destination -> done)]
+                  [going-down : (halt -> idle)
+                                (hit-bottom -> at-bottom)]
+                  [going-up : (halt -> idle)
+                              (hit-top -> at-top)]
+                  [at-top : (go-down -> going-down)
+                            (at-destination -> done)]
+                  [at-bottom : (go-up -> going-up)
+                               (at-destination -> done)]
+                  [done : accept]))
+
+(elevator '(go-up halt go-down hit-bottom at-destination)) ; true
+
+; Test:
+; match c(a|d)*r regex
 (define m (automaton init
-                    [init : (c -> more)]
-                    [more : (a -> more)
-                            (d -> more)
-                            (r -> end)]
-                    [end : accept]))
-(m '(c a d a d r))
-(m '(c a d a d r a))
+                     [init : (c -> more)]
+                     [more : (a -> more)
+                             (d -> more)
+                             (r -> end)]
+                     [end : accept]))
 
-(defautomaton m2
-  init
-  [init : (c -> more)]
-  [more : (a -> more)
-          (d -> more)
-          (r -> end)]
-  [end : accept])
+(m '(c a d a d r))   ; true
+(m '(c a d a d r a)) ; false
 
-(m2 '(c a d a d r))
-(m2 '(c a d a d r a))
+                                         
--- tests/automatonmk.rkt
+++ tests/automatonmk.rkt
@@ -1,13 +1,43 @@
-;; Test
+#lang racket
+
+(require "../automatonmk.rkt"
+         minikanren)
+
+; Test:
+; Example from stackoverflow
+; https://codereview.stackexchange.com/questions/143726/event-driven-finite-state-machine-dsl-in-kotlin
+(define elevator (automaton/mk
+                  idle
+                  [idle : ('go-up   -> going-up)
+                          ('go-down -> going-down)
+                          ('at-destination -> done)]
+                  [going-down : ('halt -> idle)
+                                ('hit-bottom -> at-bottom)]
+                  [going-up : ('halt -> idle)
+                              ('hit-top -> at-top)]
+                  [at-top : ('go-down -> going-down)
+                            ('at-destination -> done)]
+                  [at-bottom : ('go-up -> going-up)
+                               ('at-destination -> done)]
+                  [done : accept]))
+
+; What's interesting about the miniKanren version is that it can generate valid sequences:
+(run 10 (q) (elevator q #t))
+
+; Test
+; match: c(a|d)*r regex
 (define m/k
   (automaton/mk init
                 [init : ('c -> more)]
                 [more : ('a -> more)
-                       ('d -> more)
-                       ('r -> end)]
+                        ('d -> more)
+                        ('r -> end)]
                 [end : accept]))
 
-(run 10 (q) (m/k q #f))
+; Can recognize valid or invalid sequences:
 (run 1 (q) (m/k '(a c d r) q))
-(run 1 (q)  (m/k '(c a d a d r) q))
 (run 1 (q) (m/k '(c a d a d r) q))
+(run 1 (q) (m/k '(c a d a d r) q))
+
+; Can generate valid sequences
+(run 10 (q) (m/k q #t))
